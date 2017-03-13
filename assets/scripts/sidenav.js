a28708c4a9329d6ae5fe752508e873edca27563c
Eric Killham <ekillham@luc.edu>
2017-03-13 01:17:41 -0500

    Sign Out
    Attempted Adding Signout functionality for google

--- assets/scripts/sidenav.js
+++ assets/scripts/sidenav.js
@@ -7,3 +7,10 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+function signOut() {
+	var auth2 = gapi.auth2.getAuthInstance();
+	auth2.signOut().then(function() {
+		console.log('User has signed out.');
+	});
+	
