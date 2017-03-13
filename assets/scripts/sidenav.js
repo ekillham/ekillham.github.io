d40bbf51df5de43a2b4747fa37476366d6d55ca6
Eric Killham <ekillham@luc.edu>
2017-03-13 01:25:08 -0500

    Bug Fix
    fixed bug with slider menu

--- assets/scripts/sidenav.js
+++ assets/scripts/sidenav.js
@@ -8,12 +8,4 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
 
-function signOut() {
-	var auth2 = gapi.auth2.getAuthInstance();
-	auth2.signOut().then(function() {
-		console.log('User has signed out.');
-	}
-	
-	
-	
 	
